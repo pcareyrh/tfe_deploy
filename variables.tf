a821e23d259bcf22b069931c50af2b6b30da4c9e
Patrick <pcarey@hashicorp.com>
2021-04-30 14:20:27 +1000

    update for crt config

--- variables.tf
+++ variables.tf
@@ -109,6 +109,22 @@ variable "vm_count" {
   default = 2
 }
 
-variable "tfe_key" {
+variable "ssh_tfe_key" {
   type = string
+}
+
+
+variable "ca_public_key_file_path" {
+  description = "Write the PEM-encoded CA certificate public key to this path (e.g. /etc/tls/ca.crt.pem)."
+  default = "tls/ca.crt.pem" 
+}
+
+variable "public_key_file_path" {
+  description = "Write the PEM-encoded certificate public key to this path (e.g. /etc/tls/vault.crt.pem)."
+  default = "tls/vault.crt.pem" 
+}
+
+variable "private_key_file_path" {
+  description = "Write the PEM-encoded certificate private key to this path (e.g. /etc/tls/vault.key.pem)."
+  default = "tls/vault.key.pem" 
 }
